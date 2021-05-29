87bb5c782f8f2c3a8abdf3ca7ebaf6dbb345fa8e
lcn <lcnssamlcn@gmail.com>
2021-05-30 01:04:51 +0800

    first commit

---
+++ js/ability.js
@@ -0,0 +1,22 @@
+var IMG_DIR = "../img";
+var RATING_MIN = 0;
+var RATING_MAX = 5;
+
+function renderRating(val) {
+    var nodeFullStar = "<img class='rating-star' src='" + IMG_DIR + "/star_full.png' />";
+    var nodeEmptyStar = "<img class='rating-star' src='" + IMG_DIR + "/star_empty.png' />";
+    var nodeTotal = "";
+    for (var i = RATING_MIN; i < val; i++) {
+        nodeTotal += nodeFullStar;
+    }
+    for (var i = val; i < RATING_MAX; i++) {
+        nodeTotal += nodeEmptyStar;
+    }
+    $(nodeTotal).appendTo("span.rating-" + val);
+}
+
+$(document).ready(function () {
+    for (var i = RATING_MIN; i <= RATING_MAX; i++) {
+        renderRating(i);
+    }
+});
